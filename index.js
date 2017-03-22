3c14261ac1ef242d6e5e79954cdeab0f52a7efde
Paul Gadi <paulgadi@gmail.com>
2017-03-22 14:11:51 +0800

    added crawling cursor code

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ if (typeof AFRAME === 'undefined') {
  * Remote Phone Controls component for A-Frame.
  */
 AFRAME.registerComponent('remote-phone-controls', {
-  schema: {},
+  dependencies: ['raycaster'],
+  schema: {
+      target: {
+          type: "selector"
+      }
+  },
 
   /**
    * Set if component needs multiple instancing.
@@ -35,6 +40,51 @@ AFRAME.registerComponent('remote-phone-controls', {
         orientation.gamma = evt.gamma;
 		}, true);
 
+    var el = this.el;
+    var data = this.data;
+
+    if (data.target === null) {
+        var cursor = document.querySelector("a-cursor");
+
+        if (cursor === null) {
+            console.warn("Please put a-cursor in a document");
+            return;
+        }
+
+        data.target = cursor;
+    }
+
+    el.addEventListener("raycaster-intersection", function(e) {
+
+        var intersection = getNearestIntersection(e.detail.intersections);
+        if (!intersection) {return;}
+
+        // a matrix which represents item's movement, rotation and scale on global world
+        var mat = intersection.object.matrixWorld;
+        // remove parallel movement from the matrix
+        mat.setPosition(new THREE.Vector3(0, 0, 0));
+
+        // change local normal into global normal
+        var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
+
+        // look at target coordinate = intersection coordinate + global normal vector
+        var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
+        data.target.object3D.lookAt(lookAtTarget);
+
+        // cursor coordinate = intersection coordinate + normal vector * 0.05(hover 5cm above intersection point)
+        var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(0.05));
+        data.target.setAttribute("position", cursorPosition);
+
+        function getNearestIntersection(intersections) {
+            for (var i = 0, l = intersections.length; i < l; i++) {
+
+                // ignore cursor itself to avoid flicker && ignore "ignore-ray" class
+                if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) {continue;}
+                return intersections[i];
+            }
+            return null;
+        }
+    });    
   },
 
   /**
