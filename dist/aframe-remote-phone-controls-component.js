112ab9c7fae0459ecea8c592727d489b7a912e7f
Paul Gadi <paulgadi@gmail.com>
2017-03-23 11:40:28 +0800

    updated dist

--- dist/aframe-remote-phone-controls-component.js
+++ dist/aframe-remote-phone-controls-component.js
@@ -54,7 +54,12 @@
 	 * Remote Phone Controls component for A-Frame.
 	 */
 	AFRAME.registerComponent('remote-phone-controls', {
-	  schema: {},
+	  dependencies: ['raycaster'],
+	  schema: {
+	      target: {
+	          type: "selector"
+	      }
+	  },
 
 	  /**
 	   * Set if component needs multiple instancing.
@@ -64,7 +69,113 @@
 	  /**
 	   * Called once when component is attached. Generally for initial setup.
 	   */
-	  init: function () { },
+	  init: function () {
+
+	    // changes raycast direction
+	    this.data.raycast_rotation =
+	    {
+	      x: 0.0,
+	      y: 0.0,
+	      z: 0.0
+	    }
+
+	    // stores current and initial phone orientation
+	    this.data.orientation =
+	    {
+	      alpha: 0.0,
+	      beta: 0.0,
+	      gamma: 0.0,
+	      initial_value: null
+	    }
+
+	    this.data.raycast_initialized = false;
+
+	    // link target cursor
+	    var el = this.el;
+	    var data = this.data;
+
+	    if (data.target === null) {
+	        var cursor = document.querySelector("a-cursor");
+
+	        if (cursor === null) {
+	            console.warn("Please put a-cursor in a document");
+	            return;
+	        }
+
+	        data.target = cursor;
+	    }
+
+	    var orientation = data.orientation;
+
+	    // orientation listener
+	    window.addEventListener('deviceorientation', function (evt) {
+
+	      if (orientation.initial_value == null)
+	      {
+	          // get initial start rotation
+	          orientation.initial_value =
+	          {
+	            alpha: evt.alpha,
+	            beta: evt.beta,
+	            gamma: evt.gamma
+	          }
+
+	          data.raycast_initialized = true;
+	      }
+
+	      // store event orientation values
+	      orientation.alpha =  evt.alpha;
+	      orientation.beta = evt.beta;
+	      orientation.gamma = evt.gamma;
+
+			}, true);
+
+	    // touch to recenter cursor
+	    window.addEventListener('touchstart', function (evt) {
+
+	      orientation.initial_value =
+	      {
+	          alpha: orientation.alpha,
+	          beta: orientation.beta,
+	          gamma: orientation.gamma
+	      };
+
+			}, true);
+
+
+	    // cursor raycaster listener
+	    el.addEventListener("raycaster-intersection", function(e) {
+
+	        var intersection = getNearestIntersection(e.detail.intersections);
+	        if (!intersection) {return;}
+
+	        // a matrix which represents item's movement, rotation and scale on global world
+	        var mat = intersection.object.matrixWorld;
+	        // remove parallel movement from the matrix
+	        mat.setPosition(new THREE.Vector3(0, 0, 0));
+
+	        // change local normal into global normal
+	        var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
+
+	        // look at target coordinate = intersection coordinate + global normal vector
+	        var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
+	        data.target.object3D.lookAt(lookAtTarget);
+
+	        // cursor coordinate = intersection coordinate + normal vector * 0.05(hover 5cm above intersection point)
+	        var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(0.05));
+	        data.target.setAttribute("position", cursorPosition);
+
+	        function getNearestIntersection(intersections) {
+	            for (var i = 0, l = intersections.length; i < l; i++) {
+
+	                // ignore cursor itself to avoid flicker && ignore "ignore-ray" class
+	                if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) {continue;}
+	                return intersections[i];
+	            }
+	            return null;
+	        }
+	    });
+	  },
 
 	  /**
 	   * Called when component is attached and when component data changes.
@@ -81,7 +192,23 @@
 	  /**
 	   * Called on each scene tick.
 	   */
-	  // tick: function (t) { },
+	  tick: function (t) {
+	      var data = this.data;
+	      var el = this.el;
+
+	      if (data.raycast_initialized) {
+
+	        // rotate raycaster depending on orientation
+	        var rotation = el.getAttribute('rotation');
+
+	        rotation.y = data.orientation.alpha - data.orientation.initial_value.alpha;
+	        rotation.x = data.orientation.beta - data.orientation.initial_value.beta;
+
+	        el.setAttribute('rotation', rotation);
+
+	        rotation = el.getAttribute('rotation');
+	     }
+	  },
 
 	  /**
 	   * Called when entity pauses.
